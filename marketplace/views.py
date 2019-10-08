7a9131082529bb430924f50263d8d7f3f2285e68
Pcgeek01 <54470081+HappilyIgnorant@users.noreply.github.com>
2019-10-08 20:00:59 +1000

    Added item create route and item details route

--- marketplace/views.py
+++ marketplace/views.py
@@ -6,3 +6,11 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
      return render_template("index.html")
+
+@bp.route('/item_create')
+def item_create():
+     return render_template("item_create.html")
+
+@bp.route('/item_details')
+def item_details():
+     return render_template("item_details.html")
