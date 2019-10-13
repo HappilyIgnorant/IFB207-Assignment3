798c7e81b837212994245745990eeb24ca12b48e
justinrsh <54873120+justinrsh@users.noreply.github.com>
2019-10-13 16:55:18 +1000

    Update views.py

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from .forms import RegisterForm1, RegisterForm2, RegisterForm3
 
 bp = Blueprint('main', __name__)
 
@@ -14,3 +15,11 @@ def item_create():
 @bp.route('/item_details')
 def item_details():
      return render_template("item_details.html")
+
+@bp.route('/register')
+def register():
+     form1 = RegisterForm1()
+     form2 = RegisterForm2()
+     form3 = RegisterForm3()
+     return render_template("registration.html", 
+     form1 = form1, form2 = form2, form3 = form3)
